5c88327398f5e286b5bd7983db6eb4ee79abf23b
adityatadas24 <adityatadas2000@gmail.com>
2023-12-23 19:14:09 +0530

    submitting code-Sat Dec 23 2023 19:14:07 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,69 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [user, setUser] = useState("");
+  const [noDataFound, setNoDataFound] = useState(true);
+
+
+  async function TableList() {
+    try {
+      const response = await fetch("https://reqres.in/api/users");
+
+      const data = await response.json();
+
+      if (response.ok) {
+        setUser(data.data);
+        setNoDataFound(false);
+      } else {
+        
+        setNoDataFound(true);
+      }
+    
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <nav>
+        <h3>Blue Whales</h3>
+        <button onClick={TableList}>Get User List</button>
+      </nav>
+     
+        <table>
+          <thead className="thead">
+            <tr>
+              <th>First name</th>
+              <th>Last name</th>
+              <th>Email</th>
+              <th>Avatar</th>
+             
+            </tr>
+            
+          </thead>
+         {noDataFound && <h4>no data found on display</h4>}
+          <tbody >
+            {user!=="" && (user.map((users) => (
+              <tr  key={users.id}>
+            
+                <td className="tbody">{users.first_name}</td>
+                
+                <td className="tbody">{users.last_name}</td>
+                <td className="tbody">{users.email}</td>
+                <td className="tbody">
+                  <img src={users.avatar} alt={user.first_name} />
+                </td>
+                
+              </tr>
+                
+            )))}
+          
+          </tbody>
+        </table>
+    
     </div>
   )
 }
