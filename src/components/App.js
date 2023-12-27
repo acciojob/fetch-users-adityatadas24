defb39e91ba3ef72aa7c7ff72274d1eefd64f31f
adityatadas24 <adityatadas2000@gmail.com>
2023-12-27 21:50:39 +0530

    submitting code-Wed Dec 27 2023 21:50:37 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,71 +1,64 @@
-
-import React,{useState} from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./../styles/App.css";
 
 const App = () => {
   const [user, setUser] = useState("");
-  const [noDataFound, setNoDataFound] = useState(true);
 
-
-  async function TableList() {
-    try {
-      const response = await fetch("https://reqres.in/api/users");
-
-      const data = await response.json();
-
-      if (response.ok) {
-        setUser(data.data);
-        setNoDataFound(false);
-      } else {
-        
-        setNoDataFound(true);
-      }
-    
-    } catch (error) {
-      console.log(error);
-    }
+  function TableList() {
+    axios
+      .get("https://reqres.in/api/users")
+      .then((response) => {
+        setUser(response.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
   return (
     <div>
-        {/* Do not remove the main div */}
-        <nav>
-        <h3>Blue Whales</h3>
-        <button className="btn" onClick={TableList}>Get User List</button>
-      </nav>
-     
-        <table>
-          <thead className="thead">
-            <tr>
-              <th>First name</th>
-              <th>Last name</th>
-              <th>Email</th>
-              <th>Avatar</th>
-             
-            </tr>
-            
-          </thead>
-         {noDataFound && <h4>No data found on display.</h4>}
-          <tbody >
-            {user.length>0 && (user.map((users) => (
-              <tr  key={users.id}>
-            
+      {/* Do not remove the main div */}
+
+      <span>Blue Whales</span>
+      <button className="btn" onClick={TableList}>
+        Get User List
+      </button>
+
+      <table>
+        <thead className="thead">
+          <tr>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>Email</th>
+            <th>Avatar</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {user.length > 0 ? (
+            user.map((users) => (
+              <tr key={users.id}>
                 <td className="tbody">{users.first_name}</td>
-                
+
                 <td className="tbody">{users.last_name}</td>
                 <td className="tbody">{users.email}</td>
                 <td className="tbody">
-                  <img src={users.avatar} alt={user.first_name+ " " + user.last_name} />
+                  <img
+                    src={users.avatar}
+                    alt={user.first_name + " " + user.last_name}
+                  />
                 </td>
-                
               </tr>
-                
-            )))}
-          
-          </tbody>
-        </table>
-    
+            ))
+          ) : (
+            <tr>
+              <h4>No data found on display.</h4>
+            </tr>
+          )}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
